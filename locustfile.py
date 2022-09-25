53d522ae7fce5281dc9fca83bfb9e9ea5c415982
lorenzo <lorenzo.maddalena1@gmail.com>
2022-09-25 22:01:06 +0200

    change locust

--- locustfile.py
+++ locustfile.py
@@ -4,4 +4,4 @@ class HelloWorldUser(HttpUser):
     @task
     def hello_world(self):
         self.client.get("/")
-        self.client.post("/predict")
+        self.client.post("/predict", json = {"CHAS": {"0": 0}, "RM": {"0": 6.575}, "TAX": {"0": 296.0}, "PTRATIO": {"0": 15.3}, "B": {"0": 396..9}, "LSTAT": {"0": 4.98}})
